d2f616f9eba690e649722372fd55ca78e4689cd6
Gonçalves Alves <48800908+lucasgoncalvesealves@users.noreply.github.com>
2024-10-30 22:16:23 -0300

    Add files via upload

---
+++ NomeSobrenomeIdade.rb
@@ -0,0 +1,7 @@
+print "Digite seu nome: "
+nome = gets.chomp
+print "Digite seu sobrenome: "
+sobrenome = gets.chomp
+print "Digite sua idade: "
+idade = gets.chomp.to_i
+puts "Seu nome completo é #{nome} #{sobrenome}, e você tem #{idade} anos."
